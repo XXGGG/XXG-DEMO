cb9cc369e8bc51b1b7e867193f5769764ad37cff
谢夏戈 <534464179@qq.com>
2022-06-18 05:11:21 +0800

    test

--- histoire.config.js
+++ histoire.config.js
@@ -1,9 +1,29 @@
-/// <reference types="histoire" />
-
-import { defineConfig } from 'vite'
-import { defaultColors } from 'histoire'
+import { defineConfig } from 'histoire'
 
 export default defineConfig({
+  histoire: {
+    // your Histoire configuration
+  },
+  tree: {
+    groups: [
+      {
+        id: 'top',
+        title: '', // No toggle
+      },
+      {
+        title: 'My Group',
+        include: file => /Code gen|Controls|Docs/.test(file.title),
+      },
+      {
+        title: 'Components',
+        include: file => !file.title.includes('Serialize'),
+      },
+      {
+        title: 'Others',
+        include: file => true,
+      },
+    ],
+  },
   theme: {
     title: 'XG-DEMO',
     logoHref: 'https://github.com/XXGGG',
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+/// <reference types="histoire" />
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'/',
+  histoire: {
+    // your Histoire configuration
+  },
 })
